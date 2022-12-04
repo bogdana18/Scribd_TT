855ad714797a0c7f9138d828973d05f251a8956c
Usenko Bohdana <usenkobogdana19@gmail.com>
2022-12-04 16:45:29 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--menu-is-open');
+  } else {
+    document.body.classList.remove('page__body--menu-is-open');
+  }
+});
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  e.target.reset();
+});
